edb1c4a46bf6983ec48ed7ed28a54ea7eb58eb94
nova-sangeeth <novasangeeth@outlook.com>
2020-09-17 00:03:03 +0530

    added new method for handling the errors.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,7 +9,22 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  handleError = (error: HttpErrorResponse) => {
+    let errorMessage = 'Unknown-Errors';
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  // public sendGetRequest() {
+  //   return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
+  // }
   public get() {
-    return this.httpClient.get(this.SERVER_URL);
+    return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
   }
 }
