a27efbb1c89ffe12c1eb325338d37b833f68e1c4
nova-sangeeth <novasangeeth@outlook.com>
2020-09-17 19:29:56 +0530

    lowered the limit from 300 to 200

--- server/generate.js
+++ server/generate.js
@@ -2,7 +2,7 @@ var faker = require('faker');
 
 var database = { products: [] };
 
-for (var i = 1; i <= 300; i++) {
+for (var i = 1; i <= 200; i++) {
     database.products.push({
         id: i,
         name: faker.commerce.productName(),
